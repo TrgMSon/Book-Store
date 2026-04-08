5f07bc19b4f75989188bc12e1137c2d53d381ce9
TrgMSon <truongminhson1810@gmail.com>
2026-04-08 21:50:18 +0700

    update local time

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,10 +1,19 @@
 package com.example.bookstore;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class BookstoreApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
